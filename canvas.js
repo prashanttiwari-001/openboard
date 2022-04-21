dafecca8a8d01642505fff1dbfaf8f34f733b90c
prashanttiwari-001 <prashantttiwari976@gmail.com>
2022-04-21 13:19:46 +0530

    download work

--- canvas.js
+++ canvas.js
@@ -5,6 +5,7 @@ canvas.height = window.innerHeight;
 let pencilColor = document.querySelectorAll(".pencil-color");
 let pencilWidthElem = document.querySelector(".pencil-width");
 let eraserWidthElem = document.querySelector(".eraser-width");
+let download = document.querySelector(".download");
 
 let penColor = "red";
 let eraserColor = "white";
@@ -35,18 +36,32 @@ tool.lineWidth = penWidth;
 // mousedown -> start new path, mousemove fill (graphics)
 canvas.addEventListener("mousedown",function(e){
     mouseDown = true;
-    tool.beginPath();
-    tool.moveTo(e.clientX, e.clientY);
+    beginPath({
+        x: e.clientX,
+        y: e.clientY
+    })
 })
 canvas.addEventListener("mousemove",function(e){
-   if(mouseDown){
-    tool.lineTo(e.clientX,e.clientY);
-    tool.stroke();
-   }
+   if(mouseDown) drawStroke({
+    x: e.clientX,
+    y: e.clientY,
+    color: eraserFlag ? eraserColor : penColor,
+    width: eraserFlag ? eraserWidth : penWidth
+   })
 })
 canvas.addEventListener("mouseup",(e)=>{
     mouseDown = false;
 })
+function beginPath(strokeObj){
+    tool.beginPath();
+    tool.moveTo(strokeObj.x, strokeObj.y);
+}
+function drawStroke(strokeObj){
+    tool.strokeStyle = strokeObj.color;
+    tool.lineWidth = strokeObj.width;
+    tool.lineTo(strokeObj.x,strokeObj.y);
+    tool.stroke();
+}
 pencilColor.forEach((colorElem) => {
     colorElem.addEventListener("click",(e)=>{
         let color = colorElem.classList[0];
@@ -71,3 +86,13 @@ eraser.addEventListener("click",(e)=>{
         tool.lineWidth = penWidth;
     }
 })
+
+download.addEventListener("click",function(e){
+   let url = canvas.toDataURL();
+   
+    let a = document.createElement("a");
+    a.href = url;
+    a.download = "board.jpg";
+    a.click();
+})
+
